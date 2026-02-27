b1b7d2fccda5d979ae42eb9a92cff713b8b9865f
gayathrisathish <gayathri.sathish@hotmail.com>
2026-02-27 09:33:56 +0530

    UC1: Add welcome message module

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,2 +1,20 @@
+
 public class PalindromeCheckerApp {
+
+    /**
+     * Application entry point.
+     *
+     * This is the first method executed by the JVM
+     * when the program starts.
+     *
+     * @param args Command-line arguments
+     */
+    public static void main(String[] args) {
+
+
+        System.out.println("   Welcome to Palindrome Checker App ");
+        System.out.println("Application Name : Palindrome Checker");
+        System.out.println("Version          : 1.0");
+        System.out.println("System initialized successfully");
+    }
 }
