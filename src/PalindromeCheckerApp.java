d38bff20bff59f58016f676ebaccba909b28b483
gayathrisathish <gayathri.sathish@hotmail.com>
2026-02-27 09:53:34 +0530

    UC2:Hardcoded palindrome

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -2,19 +2,38 @@
 public class PalindromeCheckerApp {
 
     /**
-     * Application entry point.
-     *
-     * This is the first method executed by the JVM
-     * when the program starts.
+     * Application entry point for UC3.
      *
      * @param args Command-line arguments
      */
     public static void main(String[] args) {
 
+        System.out.println("Palindrome Checker - UC3");
 
-        System.out.println("   Welcome to Palindrome Checker App ");
-        System.out.println("Application Name : Palindrome Checker");
-        System.out.println("Version          : 1.0");
-        System.out.println("System initialized successfully");
+
+        // Hardcoded string
+        String original = "racecar";
+
+        System.out.println("Original String: " + original);
+
+        // String to store reversed value
+        String reversed = "";
+
+        // Reverse string using for loop
+        for (int i = original.length() - 1; i >= 0; i--) {
+            reversed = reversed + original.charAt(i);
+        }
+
+        System.out.println("Reversed String: " + reversed);
+
+        // Compare using equals() method
+        if (original.equals(reversed)) {
+            System.out.println(original + " is a Palindrome.");
+        } else {
+            System.out.println(original + " is NOT a Palindrome.");
+        }
+
+
+        System.out.println("Program Ended.");
     }
 }
